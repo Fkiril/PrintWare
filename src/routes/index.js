4ec4d980f702fa366dbfb188a41365c395d3e925
namenguyn <168397134+namenguyn@users.noreply.github.com>
2024-11-26 10:17:08 +0700

    Delete src directory

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const spsoRouter = require('./spso')
-function route(app){
-    // app.get('/', (req, res) => {
-    //     res.send('Hello World!')
-    //   })
-    app.use('/spso', spsoRouter)
-}
-
-module.exports = route
