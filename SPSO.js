5e58dc4d330a3a063fb71559d7d32b864c2bdd70
Nam Nguyễn <168397134+namenguyn@users.noreply.github.com>
2024-12-05 10:03:11 +0700

    1 số code về Systemconfig đc clean

---
+++ SPSO.js
@@ -0,0 +1,13 @@
+import { firestore } from '../services/FirebaseAdminSDK.js';
+
+
+import SystemConfig from './SystemConfig.js';
+
+
+export default class SPSO {
+    constructor() {
+        this.systemConfig = new SystemConfig();
+        this.history = [];
+        this.printers = [];
+        this.rooms = [];
+    } }
